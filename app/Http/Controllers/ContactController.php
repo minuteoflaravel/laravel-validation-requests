fef26c6088a95586450857ed84719d874e479312
minuteoflaravel <contact@minuteoflaravel.com>
2021-10-08 18:04:28 +0300

    Remove validation from controller & type-hint ContactRequest

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class ContactController extends Controller
 {
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|max:100',
-            'email' => 'required|email|max:255|unique:contacts',
-        ]);
-
         Contact::create($request->only(['name','email']));
 
         Session::flash('message', 'Contact was created successfully!');
