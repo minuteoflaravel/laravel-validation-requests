fb38a0caaddf518ebbae0066765a87b0fe880966
minuteoflaravel <contact@minuteoflaravel.com>
2021-10-08 16:01:50 +0300

    Fix form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,24 +19,25 @@
 
                     @if ($errors->any())
                         @foreach ($errors->all() as $error)
-                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mb-4 rounded relative" role="alert">
                                 <span class="block sm:inline">{{ $error }}</span>
                             </div>
                         @endforeach
                     @endif
 
-                    <form method="POST" action="" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <form method="POST" action="{{route('contact.store')}}" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                        @csrf
                         <div class="mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
                                 Name
                             </label>
-                            <input id="name" type="text" placeholder="Name" value="{{old('name')}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" >
+                            <input id="name" name="name" type="text" placeholder="Name" value="{{old('name')}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" >
                         </div>
                         <div class="mb-6">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                                 Email
                             </label>
-                            <input id="email" type="text" placeholder="Email" value="{{old('email')}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" >
+                            <input id="email" name="email" type="text" placeholder="Email" value="{{old('email')}}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" >
                         </div>
 
                         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
