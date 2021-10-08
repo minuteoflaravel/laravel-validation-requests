c0557f07300daa9520ab85ecc640a1749654158f
minuteoflaravel <contact@minuteoflaravel.com>
2021-10-08 15:47:48 +0300

    Flash success message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,6 +11,12 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1 class="text-xl mb-4">Create Contact</h1>
 
+                    @if(Session::has('message'))
+                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                            <span class="block sm:inline">{{ Session::get('message') }}</span>
+                        </div>
+                    @endif
+
                     @if ($errors->any())
                         @foreach ($errors->all() as $error)
                             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
